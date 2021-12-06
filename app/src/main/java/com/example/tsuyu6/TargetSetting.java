c0cd2d776c8ba3b1fa61ae46bc81c49b90f5e812
muraitsuyu <murai.tsuyu1026@gmail.com>
2021-12-06 11:58:37 +0900

    12/6 2限

---
+++ app/src/main/java/com/example/tsuyu6/TargetSetting.java
@@ -0,0 +1,14 @@
+package com.example.tsuyu6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TargetSetting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_target_setting);
+    }
+}
